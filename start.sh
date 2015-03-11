3eb66e92d54182a4cccfba9e298a09543897e43c
mgm <mgm@aut-aut.hr>
2015-03-11 02:46:55 +0100

    type in docker config.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker stop croFxRest
-docker rm croFxRest
-docker run -d -p 5006:5000 -e mydockerhost=`hostname -f` --restart=on-failure:10 --name croFxRest dkrs.co/croFxRest
+docker stop crofxrest
+docker rm crofxrest
+docker run -d -p 5006:5000 -e mydockerhost=`hostname -f` --restart=on-failure:10 --name croFxRest dkrs.co/crofxrest
